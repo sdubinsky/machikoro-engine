7b945d407733f8bf0c96e699d31067253c466fb4
Scott Dubinsky <smdubinsky@gmail.com>
2019-10-20 00:39:56 +0300

    changes, and first few tests

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,43 +1,66 @@
-module Lib
-    (
-    ) where
+module Lib where
 import Data.List
 
 data Card = Card {cardName :: String,
-                 description :: String} deriving Show
+                  cardDescription :: String} deriving Show
+data FieldCard = FieldCard {fieldCard :: Card,
+                           fieldCardCount :: Int} deriving (Show, Eq)
 instance Eq Card where
   x == y = (cardName x == cardName y)
 
 data DiceRoll = DiceRoll Int Int deriving Show
-
-type Deck = [Card]
-type Field = [Card]
-type Hand = [Card]
-type Improvements = [Card]
-type RollHistory = [DiceRoll]
-
+data Improvement = Improvement {impName :: String,
+                   impDescription :: String,
+                   impActive :: Bool} deriving Show
 
 data Player = Player {playerName :: String, hand :: Hand, cash :: Integer, improvements :: Improvements} deriving Show
 instance Eq Player where
   x == y = (playerName x == playerName y)
 
-data Board = Board {players :: [Player], deck :: Deck, field :: Field, rollHistory :: RollHistory } deriving Show
+data Board = Board {players :: [Player],
+                    deck :: Deck,
+                    field :: Field,
+                    rollHistory :: TurnHistory } deriving Show
+
+data Turn = Turn {activePlayer :: Player,
+                  roll :: DiceRoll,
+                  cardBought :: Card
+                 } deriving Show
+
+type Deck = [Card] -- the deck we draw from
+type Field = [FieldCard] -- the ten faceup cards
+type Hand = [Card] -- the cards the player bought from the field
+type Improvements = [Card] -- the city improvements you buy to win
+type TurnHistory = [Turn] -- all prior turns
 
 moveCardToPlayer :: Card -> Player -> Board -> Board
 updatePlayerList :: Player -> [Player] -> [Player]
-
+addCardToField :: Card -> Field -> Field
 
 moveCardToPlayer card player board =
   let newCard = head $ deck board
       newDeck = tail $ deck board
-      newField = newCard : (delete card $ field board)
+      newField = addCardToField newCard $ deleteCardFromField card $ field board
       newHand = card : hand player
       newPlayer = player { hand = newHand}
       newPlayers = updatePlayerList newPlayer $ players board
       newBoard = board { deck = newDeck, field = newField}
       in newBoard
 
-updatePlayerList player players
-  | players == [] = []
-  | player == head players = player : tail players
-  | otherwise = head players : (updatePlayerList player $ tail players)
+updatePlayerList _ [] = []
+deleteFromPlayerList player (x:xs)
+  | player == x = player : xs
+  | otherwise = x : (updatePlayerList player $ xs)
+
+addCardToField card [] = [FieldCard { fieldCard = card, fieldCardCount = 1}]
+addCardToField card (x:xs) 
+  | card == fieldCard x = FieldCard { fieldCard = fieldCard x, fieldCardCount = fieldCardCount x + 1} : xs
+  | otherwise = x : addCardToField card xs
+
+deleteCardFromField _ [] = []
+deleteCardFromField card (x:xs)
+  | card == fieldCard x = if fieldCardCount x > 1
+                        then x {fieldCardCount = fieldCardCount x - 1} : xs
+                        else xs
+  | otherwise = x : deleteCardFromField card xs
+
---
+++ test/Spec.hs
@@ -0,0 +1,13 @@
+import Lib
+import Test.HUnit
+
+testCard = Card {cardName = "Test", cardDescription = "TestCard"}
+emptyField = [] :: Field
+
+testAddToField = TestCase $ assertEqual "adds new card to empty list" ([FieldCard {fieldCard = testCard, fieldCardCount = 1 }] :: Field) (addCardToField testCard emptyField)
+
+testAddToFieldTwice = TestCase $ assertEqual "adds new card to empty list" ([FieldCard {fieldCard = testCard, fieldCardCount = 2 }] :: Field) (addCardToField testCard $ addCardToField testCard emptyField)
+
+tests = TestList[TestLabel "field test 1" testAddToField, TestLabel "field test 2" testAddToFieldTwice]
+
+main = runTestTT tests
